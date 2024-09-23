d67862c46dba18f4d9cab00917d8eb3c476f7223
Engin <enginaygen77@gmail.com>
2024-09-23 11:28:05 +0300

    Added New Input System
    Implementing new Input system temporarily

--- Argon Assault/Assets/Scripts/PlayerController.cs
+++ Argon Assault/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] InputAction movement;
     void Start()
     {
         
     }
 
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+        //float horizontalThrow = Input.GetAxis("Horizontal");
+        //float verticalThrow = Input.GetAxis("Vertical");
         Debug.Log(horizontalThrow);
         Debug.Log(verticalThrow);
     }
