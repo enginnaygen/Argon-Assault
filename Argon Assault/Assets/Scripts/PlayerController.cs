5f767b463529e05ba90987f0e4632586a3cbbe6b
Engin <enginaygen77@gmail.com>
2024-09-23 13:01:33 +0300

    Back To Old Input Sytem

--- Argon Assault/Assets/Scripts/PlayerController.cs
+++ Argon Assault/Assets/Scripts/PlayerController.cs
@@ -1,34 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
     void Start()
     {
         
     }
 
-    private void OnEnable()
-    {
-        movement.Enable();
-    }
-
-    private void OnDisable()
-    {
-        movement.Disable();
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
-        //float horizontalThrow = Input.GetAxis("Horizontal");
-        //float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(horizontalThrow);
-        Debug.Log(verticalThrow);
+   
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        transform.localPosition = new Vector3
+            (transform.localPosition.x + xThrow,
+            transform.localPosition.y + yThrow,
+            transform.localPosition.z);
+        
     }
 }
