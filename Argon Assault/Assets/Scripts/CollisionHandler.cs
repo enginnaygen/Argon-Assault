55a7145ef676e4e5c81f881677019215e1a53cfd
Engin <enginaygen77@gmail.com>
2024-09-25 15:10:02 +0300

    Trigger Player Explosion

--- Argon Assault/Assets/Scripts/CollisionHandler.cs
+++ Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -5,16 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    [SerializeField] PlayerController playerControlller;
+    [SerializeField] ParticleSystem playerCollisionEffect;
     [SerializeField] float delay = 1f;
   
     private void OnTriggerEnter(Collider collision)
     {
-        playerControlller.enabled = false;
-        Invoke("ReStartScene", delay);
+        StartCrashSequence();
     }
 
-    void ReStartScene()
+    private void StartCrashSequence()
+    {
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+        playerCollisionEffect.Play();
+        Invoke("ReloadScene", delay);
+
+    }
+
+    void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
