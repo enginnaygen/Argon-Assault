a41d5bbf3ce4430776afeb2c43a2c858409c6325
Engin <enginaygen77@gmail.com>
2024-09-25 18:07:14 +0300

    Display Score with UI

--- Argon Assault/Assets/Scripts/ScoreBoard.cs
+++ Argon Assault/Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        scoreText.text = "Start";
+    }
 
     public void IncreaseScore(int IncreaseAmount)
     {
         score += IncreaseAmount;
-        Debug.Log(score);
+        scoreText.text = score.ToString("000");
+        //Debug.Log(score);
     }
 }
